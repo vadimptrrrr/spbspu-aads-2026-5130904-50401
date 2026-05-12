4172c71c58218a97161a42820db71c885dedcae6
vadim <vadikdydy@mail.ru>
2026-05-12 22:49:00 +0300

    test

---
+++ petrov.vadim/S3/HashFunc.hpp
@@ -0,0 +1,45 @@
+#ifndef HASH_FUNC_HPP
+#define HASH_FUNC_HPP
+
+#include <cstddef>
+#include <string>
+#include <utility>
+
+#include <boost/hash2/sha1.hpp>
+#include <boost/hash2/get_integral_result.hpp>
+
+namespace kondrat
+{
+  using EdgeKey = std::pair< std::string, std::string >;
+
+  struct sha1
+  {
+    size_t operator()(const std::string & str) const
+    {
+      boost::hash2::sha1_160 hash;
+
+      hash.update(str.data(), str.size());
+
+      return boost::hash2::get_integral_result< size_t >(hash);
+    }
+  };
+
+  struct edgeSha1
+  {
+    size_t operator()(const EdgeKey & edge) const
+    {
+      boost::hash2::sha1_160 hash;
+
+      hash.update(edge.first.data(), edge.first.size());
+
+      const char sep = '\0';
+      hash.update(&sep, 1);
+
+      hash.update(edge.second.data(), edge.second.size());
+
+      return boost::hash2::get_integral_result< size_t >(hash);
+    }
+  };
+}
+
+#endif
--- petrov.vadim/S3/main.cpp
+++ petrov.vadim/S3/main.cpp
@@ -1,2 +1,10 @@
-int main()
-{}
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  if (argc != 1)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+}
