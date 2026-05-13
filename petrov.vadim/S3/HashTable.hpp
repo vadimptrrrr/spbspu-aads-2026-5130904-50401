944eb54d02dec23e88156f32324e390352ba7e3b
vadim <vadikdydy@mail.ru>
2026-05-13 03:56:47 +0300

    base methods decloration

--- petrov.vadim/S3/HashTable.hpp
+++ petrov.vadim/S3/HashTable.hpp
@@ -15,10 +15,22 @@ namespace petrov
   template< class Key, class Value, class Hash, class Equal >
   struct HashTable
   {
+    HashTable();
+    HashTable(size_t cap);
+    ~HashTable();
+    HashTable(const HashTable& other);
+    HashTable(HashTable&& other) noexcept;
+    HashTable& operator=(const HashTable& other);
+    HashTable& operator=(HashTable&& other) noexcept;
+
     void add(const Key& k, const Value& v);
     Value drop(const Key& k);
     bool has(const Key& k);
+    Value& get(const Key& k);
+    const Value& get(const Key& k) const;
+
     void rehash(size_t slots);
+    void clear();
 
     private:
       HashNode<Key, Value>* data_;
