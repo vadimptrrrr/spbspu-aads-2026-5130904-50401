c3ec2921cd7b80bea110f3b267c78a7a1ffa1e5b
vadim <vadikdydy@mail.ru>
2026-05-15 13:28:16 +0300

    implementation for Iterator

--- petrov.vadim/S3/HIter.hpp
+++ petrov.vadim/S3/HIter.hpp
@@ -2,6 +2,7 @@
 #define HITER_HPP
 
 #include "HashTable.hpp"
+#include <cassert>
 
 namespace petrov
 {
@@ -17,19 +18,91 @@ namespace petrov
     HIter();
 
     HIter< Key, Value >& operator++();
-    HIter< Key, Value >& operator--();
+    HIter< Key, Value >& operator++(int);
     bool operator==(const HIter< Key, Value >& rhs) const;
     bool operator!=(const HIter< Key, Value >& rhs) const;
 
     HashNode< Key, Value >& operator*() const;
-    HashNode< Key, Value >* operator*() const;
+    HashNode< Key, Value >* operator->() const;
 
     private:
-      HashNode< Key, Value >* hode_;
+      HashNode< Key, Value >* node_;
+      HashNode< Key, Value >* end_;
 
-      HTIter(HashNode< Key, Value > * node, HashNode< Key, Value > * end);
+      HIter(HashNode< Key, Value > * node, HashNode< Key, Value > * end);
       void skipInvalid();
   };
-}
 
+  template< class Key, class Value >
+  HIter< Key, Value >::HIter():
+    node_{nullptr}
+  {}
+
+  template< class Key, class Value >
+  HIter< Key, Value >& HIter< Key, Value >::operator++()
+  {
+    assert(node_ != nullptr);
+
+    if (node_ != end_)
+    {
+      ++node_;
+      skipInvalid();
+    }
+    return *this;
+  }
+
+  template< class Key, class Value >
+  HIter< Key, Value >& HIter< Key, Value >::operator++(int)
+  {
+    HIter< Key, Value > cpy = *this;
+    ++(*this);
+    return cpy;
+  }
+
+  template< class Key, class Value >
+  bool HIter< Key, Value >::operator==(const HIter< Key, Value >& rhs) const
+  {
+    return node_ == rhs.node_;
+  }
+
+  template< class Key, class Value >
+  bool HIter< Key, Value >::operator!=(const HIter< Key, Value >& rhs) const
+  {
+    return node_ != rhs.node_;
+  }
+
+  template< class Key, class Value >
+  HashNode< Key, Value >& HIter< Key, Value >::operator*() const
+  {
+    assert(node_ != nullptr);
+    assert(node_ != end_);
+    return *node_;
+  }
+
+  template< class Key, class Value >
+  HashNode< Key, Value >* HIter< Key, Value >::operator->() const
+  {
+    assert(node_ != nullptr);
+    assert(node_ != end_);
+    return node_;
+  }
+
+  template< class Key, class Value >
+  HIter< Key, Value >::HIter(HashNode< Key, Value > * node, HashNode< Key, Value > * end):
+    node_(node),
+    end_(end)
+  {
+    skipInvalid();
+  }
+
+  template< class Key, class Value >
+  void HIter< Key, Value >::skipInvalid()
+  {
+    while (node_ != end_ && node_->state_ != OCCUPIED)
+    {
+      ++node_;
+    }
+  }
+
+}
 #endif
