e10ce26e919751318e8b95730d9eaadc986fc926
vadim <vadikdydy@mail.ru>
2026-05-15 22:53:36 +0300

    rename

---
+++ petrov.vadim/S3/Grath.hpp
@@ -0,0 +1,42 @@
+#ifndef GRATH_HPP
+#define GRATH_HPP
+
+#include <cstddef>
+#include "HashTable.hpp"
+#include "../common/vector/top-it-vector.hpp"
+#include "HashFunc.hpp"
+
+namespace petrov
+{
+  using Weight = topit::Vector< size_t >;
+  using EdgeVec = std::pair< std::string, Weight >;
+
+  struct Grath
+  {
+    Grath();
+    Grath(const Grath& other);
+    Grath(Grath&& other) noexcept;
+
+    void addVertex(const std::string& name);
+    void addEdge(const std::string& from, const std::string& to, size_t weight);
+    void removeVertex(const std::string& name);
+    void removeEdge(const std::string& from, const std::string& to, size_t weight);
+
+    bool hasVertex(const std::string& name) const;
+    bool hasEdge(const std::string& from, const std::string& to) const;
+
+    topit::Vector< std::string > getVertices() const;
+    topit::Vector< EdgeVec > getInputEdges(const std::string& name) const;
+    topit::Vector< EdgeVec > getOutputEdges(const std::string& name) const;
+    
+    void swap(Grath& other) noexcept;
+
+    std::string name_;
+
+    private:
+      HashTable< EdgeKey, Weight, edgeSha1, Equal< EdgeKey > > edges_;
+      HashTable< std::string, bool, sha1, Equal< std::string > > vertices_;
+  };
+}
+
+#endif
