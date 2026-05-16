777667a7e8843532bafcd455b997dde851d2531e
vadim <vadikdydy@mail.ru>
2026-05-16 21:56:48 +0300

    add func manage

---
+++ petrov.vadim/S3/FuncManage.hpp
@@ -0,0 +1,28 @@
+#ifndef FUNC_MANAGE_HPP
+#define FUNC_MANAGE_HPP
+
+#include <iostream>
+#include "Grath.hpp"
+
+namespace petrov
+{
+  struct FuncManage
+  {
+    void graphs(std::ostream& out, std::istream& in, std::string str);
+    void vertexes(std::ostream& out, std::istream& in, std::string str);
+    void outbound(std::ostream& out, std::istream& in, std::string str);
+    void inbound(std::ostream& out, std::istream& in, std::string str);
+
+    void bind(std::ostream& out, std::istream& in, std::string str);
+    void cut(std::ostream& out, std::istream& in, std::string str);
+
+    void create(std::ostream& out, std::istream& in, std::string str);
+    void merge(std::ostream& out, std::istream& in, std::string str);
+    void extract(std::ostream& out, std::istream& in, std::string str);
+  
+  private:
+    HashTable< std::string, Grath, sha1, Equal< std::string > > functions_;
+  };
+}
+
+#endif
