6d027bd4d01eff3e80a5f10a4768d9a95fe9d733
vadim <vadikdydy@mail.ru>
2026-05-21 18:02:36 +0300

    add iterators

---
+++ petrov.vadim/S4/BSTIters.hpp
@@ -0,0 +1,272 @@
+#ifndef BSTITERS_HPP
+#define BSTITERS_HPP
+
+#include <utility>
+#include "TNode.hpp"
+
+namespace petyrov
+{
+  template< class Key, class Value >
+  struct BSTIterator
+  {
+    using TNode = detail::TNode< Key, Value >;
+
+    BSTIterator();
+    explicit BSTIterator(TNode* node, TNode* fake_leaf);
+
+    BSTIterator& operator++();
+    BSTIterator operator++(int);
+
+    BSTIterator& operator--();
+    BSTIterator operator--(int);
+
+    bool operator==(const BSTIterator& other) const;
+    bool operator!=(const BSTIterator& other) const;
+
+    std::pair< Key, Value >& operator*() const;
+    
+  private:
+    TNode* node_;
+    TNode* fake_leaf_;
+  };
+
+  template< class Key, class Value >
+  struct BSTCIterator
+  {
+    using TNode = detail::TNode< Key, Value >;
+
+    BSTCIterator();
+    explicit BSTCIterator(const TNode* node, const TNode* fake_leaf);
+
+    BSTCIterator& operator++();
+    BSTCIterator operator++(int);
+
+    BSTCIterator& operator--();
+    BSTCIterator operator--(int);
+
+    bool operator==(const BSTCIterator& other) const;
+    bool operator!=(const BSTCIterator& other) const;
+
+    const std::pair< Key, Value >& operator*() const;
+  
+    private:
+      const TNode* node_;
+      const TNode* fake_leaf_;
+  };
+
+  template< class Key, class Value >
+  BSTIterator< Key, Value >::BSTIterator():
+    node_(nullptr),
+    fake_leaf_(nullptr)
+  {}
+
+  template< class Key, class Value >
+  BSTIterator< Key, Value >::BSTIterator(TNode* node, TNode* fake_leaf):
+    node_(node),
+    fake_leaf_(fake_leaf)
+  {}
+
+  template< class Key, class Value >
+  BSTIterator< Key, Value >& BSTIterator< Key, Value >::operator++()
+  {
+    if (node_->right_ != fake_leaf_)
+    {
+      node_ = node_->right_;
+
+      while (node_->left_ != fake_leaf_)
+      {
+        node_ = node_->left_;
+      }
+    }
+    else
+    {
+      TNode* parent = node_->parent_;
+
+      while (parent != nullptr && node_ == parent->right_)
+      {
+        node_ = parent;
+        parent = parent->parent_;
+      }
+
+      node_ = parent ? parent : fake_leaf_;
+    }
+
+    return *this;
+  }
+
+  template< class Key, class Value >
+  BSTIterator< Key, Value > BSTIterator< Key, Value >::operator++(int)
+  {
+    BSTIterator temp = *this;
+    ++(*this);
+    return temp;
+  }
+
+  template< class Key, class Value >
+  BSTIterator< Key, Value >& BSTIterator< Key, Value >::operator--()
+  {
+    if (node_ == fake_leaf_)
+    {
+      return *this;
+    }
+
+    if (node_->left_ != fake_leaf_)
+    {
+      node_ = node_->left_;
+
+      while (node_->right_ != fake_leaf_)
+      {
+        node_ = node_->right_;
+      }
+    }
+    else
+    {
+      TNode* parent = node_->parent_;
+
+      while (parent != nullptr && node_ == parent->left_)
+      {
+        node_ = parent;
+        parent = parent->parent_;
+      }
+
+      node_ = parent ? parent : fake_leaf_;
+    }
+
+    return *this;
+  }
+
+  template< class Key, class Value >
+  BSTIterator< Key, Value > BSTIterator< Key, Value >::operator--(int)
+  {
+    BSTIterator temp = *this;
+    --(*this);
+    return temp;
+  }
+
+  template< class Key, class Value >
+  bool BSTIterator< Key, Value >::operator==(const BSTIterator& other) const
+  {
+    return node_ == other.node_;
+  }
+
+  template< class Key, class Value >
+  bool BSTIterator< Key, Value >::operator!=(const BSTIterator& other) const
+  {
+    return *this != other;
+  }
+
+  template< class Key, class Value >
+  std::pair< Key, Value >& BSTIterator< Key, Value >::operator*() const
+  {
+    return {node_->key_, node_->value_};
+  }
+
+  template< class Key, class Value >
+  BSTCIterator< Key, Value >::BSTCIterator():
+    node_(nullptr),
+    fake_leaf_(nullptr)
+  {}
+
+  template< class Key, class Value >
+  BSTCIterator< Key, Value >::BSTCIterator(const TNode* node, const TNode* fake_leaf):
+    node_(node),
+    fake_leaf_(fake_leaf)
+  {}
+
+  template< class Key, class Value >
+  BSTCIterator< Key, Value >& BSTCIterator< Key, Value >::operator++()
+  {    
+    if (node_->right_ != fake_leaf_)
+    {
+      node_ = node_->right_;
+
+      while (node_->left_ != fake_leaf_)
+      {
+        node_ = node_->left_;
+      }
+    }
+    else
+    {
+      const TNode* parent = node_->parent_;
+
+      while (parent != nullptr && node_ == parent->right_)
+      {
+        node_ = parent;
+        parent = parent->parent_;
+      }
+
+      node_ = parent ? parent : fake_leaf_;
+    }
+
+    return *this;
+  }
+
+  template< class Key, class Value >
+  BSTCIterator< Key, Value > BSTCIterator< Key, Value >::operator++(int)
+  {    
+    BSTCIterator temp = *this;
+    ++(*this);
+    return temp;
+  }
+
+  template< class Key, class Value >
+  BSTCIterator< Key, Value >& BSTCIterator< Key, Value >::operator--()
+  {    
+    if (node_ == fake_leaf_)
+    {
+      return *this;
+    }
+
+    if (node_->left_ != fake_leaf_)
+    {
+      node_ = node_->left_;
+
+      while (node_->right_ != fake_leaf_)
+      {
+        node_ = node_->right_;
+      }
+    }
+    else
+    {
+      const TNode* parent = node_->parent_;
+
+      while (parent != nullptr && node_ == parent->left_)
+      {
+        node_ = parent;
+        parent = parent->parent_;
+      }
+
+      node_ = parent ? parent : fake_leaf_;
+    }
+
+    return *this;
+  }
+
+  template< class Key, class Value >
+  BSTCIterator< Key, Value > BSTCIterator< Key, Value >::operator--(int)
+  {    
+    BSTCIterator temp = *this;
+    --(*this);
+    return temp;
+  }
+
+  template< class Key, class Value >
+  bool BSTCIterator< Key, Value >::operator==(const BSTCIterator& other) const
+  {    
+    return node_ == other.node_;
+  }
+
+  template< class Key, class Value >
+  bool BSTCIterator< Key, Value >::operator!=(const BSTCIterator& other) const
+  {
+    return *this != other;
+  }
+
+  template< class Key, class Value >
+  const std::pair< Key, Value >& BSTCIterator< Key, Value >::operator*() const
+  {
+    return {node_->key_, node_->value_};
+  }
+}
+
+#endif
