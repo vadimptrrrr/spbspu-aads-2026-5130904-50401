e4513bc9a97e8668a19742824db6fbb7b9a43102
Vadim Petrov <vadikdydy@mail.ru>
2026-05-26 19:54:28 -0700

    fix math

--- petrov.vadim/S2/math.hpp
+++ petrov.vadim/S2/math.hpp
@@ -4,15 +4,16 @@
 #include <climits>
 #include <cstddef>
 #include <stdexcept>
-using ll = long long;
 namespace petrov
 {
+  using ll = long long;
+
   ll plus(const ll& a, const ll& b);
   ll minus(const ll& a, const ll& b);
   ll mult(const ll& a, const ll& b);
   ll div(const ll& a, const ll& b);
   ll mod(const ll& a, const ll& b);
-  ll reverseNumber(ll num);
+  ll reverseNumber(const ll& num);
 }
 
 #endif
