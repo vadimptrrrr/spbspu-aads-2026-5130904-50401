178a5a6dd9aaa0aaa9d23f1cb30614ae1f9725f8
Vadim Petrov <vadikdydy@maol.ru>
2026-06-18 16:24:09 +0300

    some methods and hash function

--- petrov.vadim/F0/hash_func.hpp
+++ petrov.vadim/F0/hash_func.hpp
@@ -2,11 +2,13 @@
 #define HASH_FUNC_HPP
 
 #include <cstddef>
-#include <boost/functional/hash.hpp>
+#include <string>
+#include <boost/hash2/fnv1a.hpp>
+#include <boost/hash2/get_integral_result.hpp>
 
 namespace petrov
 {
-  template <class Key>
+  template < class Key >
   struct KKHash
   {
     size_t seed_;
@@ -17,9 +19,29 @@ namespace petrov
 
     size_t operator()(const Key& key) const
     {
-      size_t hash_val = seed_;
-      boost::hash_combine(hash_val, key);
-      return hash_val;
+      boost::hash2::fnv1a_64 hash;
+      hash.update(reinterpret_cast< const char* >(&seed_), sizeof(seed_));
+      hash.update(reinterpret_cast< const char* >(&key), sizeof(key));
+
+      return boost::hash2::get_integral_result< size_t >(hash);
+    }
+  };
+
+  template <>
+  struct KKHash< std::string >
+  {
+    size_t seed_;
+
+    KKHash(size_t seed = 0):
+      seed_(seed)
+    {}
+
+    size_t operator()(const std::string& key) const
+    {
+      boost::hash2::fnv1a_64 hash;
+      hash.update(reinterpret_cast< const char* >(&seed_), sizeof(seed_));
+      hash.update(key.data(), key.size());
+      return boost::hash2::get_integral_result< size_t >(hash);
     }
   };
 }
--- petrov.vadim/F0/kuku_hash_table.hpp
+++ petrov.vadim/F0/kuku_hash_table.hpp
@@ -7,6 +7,18 @@
 
 namespace petrov
 {
+  struct Finder
+  {
+    size_t idx_;
+    bool found_;
+  };
+
+  template< class T >
+  struct Equal
+  {
+    bool operator()(const T& lhs, const T& rhs) const;
+  };
+
   template< class Value, class Key, class Hash, class Equal > 
   struct KukuHashTable
   {
@@ -46,12 +58,149 @@ namespace petrov
       Hash hash2_;
       Equal equal_;
       double load_;
-      size_t kicks_;
+      size_t maxKicks_;
 
       size_t h1(const Key& key) const;
-      size_t h2(const Key& key) const;
-      Finder find(const Key& k) const;
+      size_t h2(const Key& key) const; 
+      Finder find(const Key& k, size_t data_id) const;
   };
+
+  template< class T >
+  bool Equal< T >::operator()(const T& lhs, const T& rhs) const
+  {
+    return lhs == rhs;
+  }
+
+  template< class V, class K, class H, class E >
+  KukuHashTable< V, K, H, E >::KukuHashTable():
+    data1_(topit::Vector< detail::HashNode< K, V > >(16)),
+    data2_(topit::Vector< detail::HashNode< K, V > >(16)),
+    size_(0),
+    capacity_(32),
+    hash1_(),
+    hash2_(),
+    equal_(),
+    load_(),
+    maxKicks_(50)
+  {}
+
+  template< class V, class K, class H, class E >
+  KukuHashTable< V, K, H, E >::KukuHashTable(size_t cap):
+    data1_(topit::Vector< detail::HashNode< K, V > >(cap / 2)),
+    data2_(topit::Vector< detail::HashNode< K, V > >(cap - cap / 2)),
+    size_(0),
+    capacity_(32),
+    hash1_(),
+    hash2_(),
+    equal_(),
+    load_(),
+    maxKicks_(50)
+  {}
+
+  template< class V, class K, class H, class E >
+  KukuHashTable< V, K, H, E >::~KukuHashTable()
+  {
+    clear();
+  }
+
+  template< class V, class K, class H, class E >
+  KukuHashTable< V, K, H, E >::KukuHashTable(const KukuHashTable& other):
+    data1_(other.data1_),
+    data2_(other.data2_),
+    size_(other.size_),
+    capacity_(other.size_),
+    hash1_(other.hash1_),
+    hash2_(other.hash2_),
+    equal_(other.equal_)
+  {}
+
+  template< class V, class K, class H, class E >
+  KukuHashTable< V, K, H, E >::KukuHashTable(KukuHashTable&& other) noexcept:
+    data1_(other.data1_),
+    data2_(other.data2_),
+    size_(other.size_),
+    capacity_(other.size_),
+    hash1_(other.hash1_),
+    hash2_(other.hash2_),
+    equal_(other.equal_)
+  {
+    other.data1_.~Vector();
+    other.data2_.~Vector();
+    other.size_ = 0;
+    other.capacity_ = 0;
+  }
+
+  template< class V, class K, class H, class E >
+  KukuHashTable< V, K, H, E >& KukuHashTable< V, K, H, E >::operator=(const KukuHashTable& other)
+  {
+    if (this == std::addressof(other))
+    {
+      return *this;
+    }
+    KukuHashTable< V, K, H, E > cpy = other;
+    swap(cpy);
+    return *this;
+  }
+
+
+  template< class V, class K, class H, class E >
+  KukuHashTable< V, K, H, E >& KukuHashTable< V, K, H, E >::operator=(KukuHashTable&& other) noexcept
+  {
+    if (this == std::addressof(other))
+    {
+      return *this;
+    }
+    KukuHashTable< K, V, H, E> cpy(std::move(other));
+    swap(cpy);
+    return *this;
+  }
+
+  template< class V, class K, class H, class E >
+  void KukuHashTable< V, K, H, E >::add(const K& k, const V& v)
+  {
+    if (load_ > 0.5)
+    {
+      rehash();
+    }
+
+    if (has(k))
+    {
+      get(k) = v;
+      return;
+    }
+
+    K curr_k = k;
+    V curr_v = v;
+    for (size_t i = 0; i < maxKicks_; ++i)
+    {
+      Finder f = find(curr_k, 1);
+      if (f.found_)
+      {
+        data1_[f.idx_].value_ = curr_v;
+        data1_[f.idx_].key_ = curr_k;
+        data1_[f.idx_].state_ = OCCUPIED;
+        ++size_;
+        return;
+      }
+      std::swap(curr_k, data1_[idx1].key_);
+      std::swap(curr_v, data1_[idx1].value_);
+
+      f = find(curr_k, 2);
+      if (f.found_)
+      {
+        data2_[f.idx_].value_ = curr_v;
+        data2_[f.idx_].key_ = curr_k;
+        data2_[f.idx_].state_ = OCCUPIED;
+        ++size_;
+        return;
+      }
+      std::swap(curr_k, data1_[idx1].key_);
+      std::swap(curr_v, data1_[idx1].value_);
+    }
+
+    rehash(capacity_ * 2);
+    add(curr_k, curr_v);
+  }
 }
 
 
